8ea3fbec51f9cdfe44a1c80da20ec8b4d87d16ce
Sara Hwang <sha173@sfu.ca>
2023-07-07 20:41:33 -0700

    AI will not guess same square twice

--- ai.py
+++ ai.py
@@ -22,11 +22,18 @@ class AI():
     def run(self):
         print(self._size)
 
-    def guess(self) -> bool:
+    def guess(self):
         sleep(1)
-        x = random.randint(0, self.__size-1)
-        y = random.randint(0, self.__size-1)
-        return x, y
+        while True:
+            print("generating")
+            x = random.randint(0, self.__size-1)
+            y = random.randint(0, self.__size-1)
+            if (x, y) not in self.guessed:
+                self.guessed.append((x,y))
+                return x, y
+
+    def handleResult(self, isHit):
+        pass
 
     def place_ships(self) -> bool:
         pass
