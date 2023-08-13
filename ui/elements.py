8509e3f176be95e0d2b6af485baaef5a5ff54c63
Sara Hwang <sha173@sfu.ca>
2023-08-13 12:44:47 -0700

    Add a selected confirm button with yellow border

--- ui/elements.py
+++ ui/elements.py
@@ -8,6 +8,7 @@ hovered_button_image = pygame.image.load("assets/navy_button_hover.png")
 quit_button_image = pygame.image.load("assets/quit.png")
 confirm_button_image = pygame.image.load("assets/ConfirmButton.png")
 confirm_button_greyed = pygame.image.load("assets/ConfirmButtonGreyed.png")
+confirm_button_select = pygame.image.load("assets/ConfirmSelected.png")
 
 
 def make_button(x, y, text, font_size, reactive=False, image=base_button_image):
