a9475bf05963c90a4410b467e63f8570d3a66f78
Gabriel Ong <gbo1@sfu.ca>
2023-08-13 20:32:04 -0700

    converted mp3 to ogg file

--- ui/sounds.py
+++ ui/sounds.py
@@ -2,5 +2,5 @@ import pygame
 pygame.mixer.init()
 miss_sound = pygame.mixer.Sound("assets/sounds/miss.ogg")
 hit_sound = pygame.mixer.Sound("assets/sounds/hit.ogg")
-click_sound = pygame.mixer.Sound("assets/sounds/ui-click.mp3")
+click_sound = pygame.mixer.Sound("assets/sounds/ui-click.ogg")
 fire_sound = pygame.mixer.Sound("assets/sounds/fire.ogg")
